cdbbba23ea518ad7d4bfb1dc1abd98b90a5df0af
pkr187 <pankajroman187@gmail.com>
2022-09-11 21:51:19 +0530

    [pankaj] Add: Generic

---
+++ Generic2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Generic2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = { 1, 2, 3, 4, 5 };
+            double[] doubleArr = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            char[] charArr = { 'a', 'b' };
+            printArr<int>(intArr);
+            printArr<double>(doubleArr);
+            printArr<char>(charArr);
+        }
+        static void printArr<T>(T[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
